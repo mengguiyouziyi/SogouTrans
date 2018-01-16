e0fc7f89ab1faa9357f94c5db9bbeb2bbe18e34b
mengguiyouziyi <775618369@qq.com>
2018-01-16 17:39:25 +0800

    yd_api_v1:变换网卡

--- Diglossia/ltn/others/test.py
+++ Diglossia/ltn/others/test.py
@@ -1,14 +1,19 @@
 import pymysql
 import set_adsl
+from redis import StrictRedis
 
-# set_adsl.set_interface(sys.argv[1])
-set_adsl.set_interface('10.146.252.112')
-etl_conf = {'host': '114.253.241.251', 'port': 3306, 'user': 'spider', 'password': 'chenguang', 'charset': 'utf8',
-            'cursorclass': pymysql.cursors.DictCursor}
-etl = pymysql.connect(**etl_conf)
-etl.select_db('spider')
-cur = etl.cursor()
-sql = """select * from ltn limit 5"""
-cur.execute(sql)
-results = cur.fetchall()
-print(results)
+server = StrictRedis(host='10.134.9.106', decode_responses=True)
+server.sadd('aaaaa', 'aaaaa')
+v = server.spop('aaaaa')
+print(v)
+# # set_adsl.set_interface(sys.argv[1])
+# set_adsl.set_interface('10.146.252.112')
+# etl_conf = {'host': '10.142.98.91', 'port': 3306, 'user': 'spider', 'password': 'chenguang', 'charset': 'utf8',
+#             'cursorclass': pymysql.cursors.DictCursor}
+# etl = pymysql.connect(**etl_conf)
+# etl.select_db('spider')
+# cur = etl.cursor()
+# sql = """select * from ltn limit 5"""
+# cur.execute(sql)
+# results = cur.fetchall()
+# print(results)
