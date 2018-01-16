3207baace7222ac6caa07b783c37b245d237a104
mengguiyouziyi <775618369@qq.com>
2018-01-16 15:25:11 +0800

    yd_api_v1:变换网卡

--- Diglossia/ltn/cmd_yd_api.py
+++ Diglossia/ltn/cmd_yd_api.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     import set_adsl
 
     # set_adsl.set_interface(sys.argv[1])
-    set_adsl.set_interface('222.129.1.182')
+    set_adsl.set_interface('10.146.252.112')
     execute(['scrapy', 'crawl', file])
