45e66b38cceeddc7b826fea9e45c91b4d8757301
mengguiyouziyi <775618369@qq.com>
2018-01-17 11:58:35 +0800

    yd_api_v1:变换网卡

--- Diglossia/ltn/others/test.py
+++ Diglossia/ltn/others/test.py
@@ -1,5 +1,6 @@
 import pymysql
 import set_adsl
+from urllib.request import urlopen
 from redis import StrictRedis
 
 # server = StrictRedis(host='10.134.9.106', decode_responses=True)
@@ -7,13 +8,15 @@ from redis import StrictRedis
 # v = server.spop('aaaaa')
 # print(v)
 # set_adsl.set_interface(sys.argv[1])
-# set_adsl.set_interface('10.142.97.80')
-etl_conf = {'host': '10.142.98.91', 'port': 3306, 'user': 'spider', 'password': 'chenguang', 'charset': 'utf8',
-            'cursorclass': pymysql.cursors.DictCursor}
-etl = pymysql.connect(**etl_conf)
-etl.select_db('spider')
-cur = etl.cursor()
-sql = """select * from ltn limit 5"""
-cur.execute(sql)
-results = cur.fetchall()
-print(results)
+set_adsl.set_interface('10.142.97.80')
+s = urlopen('http://www.baidu.com')
+print(s)
+# etl_conf = {'host': '10.142.98.91', 'port': 3306, 'user': 'spider', 'password': 'chenguang', 'charset': 'utf8',
+#             'cursorclass': pymysql.cursors.DictCursor}
+# etl = pymysql.connect(**etl_conf)
+# etl.select_db('spider')
+# cur = etl.cursor()
+# sql = """select * from ltn limit 5"""
+# cur.execute(sql)
+# results = cur.fetchall()
+# print(results)
