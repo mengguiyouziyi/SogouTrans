07bd2c465b77f9efe3346a7ffbf6ae95f56e709b
mengguiyouziyi <775618369@qq.com>
2018-01-17 16:15:46 +0800

    yd_api_v1:变换网卡

--- Diglossia/ltn/others/test.py
+++ Diglossia/ltn/others/test.py
@@ -4,8 +4,8 @@ from urllib.request import urlopen
 from redis import StrictRedis
 
 # set_adsl.set_interface(sys.argv[1])
-# set_adsl.set_interface('10.146.252.120')
-server = StrictRedis(host='10.146.252.113', decode_responses=True)
+set_adsl.set_interface('10.146.252.120')
+server = StrictRedis(host='10.142.97.92', decode_responses=True)
 server.sadd('aaaaa', 'aaaaa')
 v = server.spop('aaaaa')
 print(v)
@@ -17,7 +17,7 @@ print(v)
 # etl = pymysql.connect(**etl_conf)
 # etl.select_db('spider')
 # cur = etl.cursor()
-# sql = """select * from ltn limit 5"""
+# sql = """select * from ltn limit 1"""
 # cur.execute(sql)
 # results = cur.fetchall()
 # print(results)
