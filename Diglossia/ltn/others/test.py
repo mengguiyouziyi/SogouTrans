bfc115e9f1308072be50542c0b1b0caa3a921323
mengguiyouziyi <775618369@qq.com>
2018-01-17 17:01:03 +0800

    yd_api_v1:变换网卡

--- Diglossia/ltn/others/test.py
+++ Diglossia/ltn/others/test.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen
 from redis import StrictRedis
 
 # set_adsl.set_interface(sys.argv[1])
-set_adsl.set_interface('10.146.252.120')
+set_adsl.set_interface('10.146.252.112')
 # server = StrictRedis(host='10.152.97.92', decode_responses=True)
 # server.sadd('aaaaa', 'aaaaa')
 # v = server.spop('aaaaa')
@@ -12,12 +12,12 @@ set_adsl.set_interface('10.146.252.120')
 
 s = urlopen('http://www.baidu.com')
 print(s)
-etl_conf = {'host': '10.142.98.91', 'port': 3306, 'user': 'spider', 'password': 'chenguang', 'charset': 'utf8',
+etl_conf = {'host': '10.146.252.112', 'port': 3306, 'user': 'spider', 'password': 'chenguang', 'charset': 'utf8',
             'cursorclass': pymysql.cursors.DictCursor}
 etl = pymysql.connect(**etl_conf)
 etl.select_db('spider')
 cur = etl.cursor()
-sql = """select * from ltn limit 1"""
+sql = """select * from yd_api_ja-zh limit 1"""
 cur.execute(sql)
 results = cur.fetchall()
 print(results)
