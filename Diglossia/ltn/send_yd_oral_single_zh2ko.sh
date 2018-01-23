f7434fe826523c39cadcf380ac7966b6249cff05
mengguiyouziyi <775618369@qq.com>
2018-01-23 10:53:35 +0800

    yd_oral_single_zh2ko

--- Diglossia/ltn/send_yd_oral_single_zh2ko.sh
+++ Diglossia/ltn/send_yd_oral_single_zh2ko.sh
@@ -9,11 +9,13 @@ cd /search/chenguang/meng/SogouTrans/Diglossia/ltn/util/
 
 self=$BASH_SOURCE
 o_file=${self/sh/out}
-out_file="/search/chenguang/meng/logs/SogouTrans/"$self
+out_file="/search/chenguang/meng/logs/SogouTrans/"$o_file
 spider_name=${self/send_/}
 spider_name=${self/.sh/}
 request_key=$spider_name":requests"
 i_file="oral800w.zh"
 in_file="/search/chenguang/meng/documents/SogouTrans/"$i_file
 
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 nohup python send.py $in_file $request_key > $out_file 2>&1 &
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
