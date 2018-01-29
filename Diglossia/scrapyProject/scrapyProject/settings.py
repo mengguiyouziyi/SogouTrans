a8ab22bbf02095974ecfca03478c66111b439454
mengguiyouziyi <775618369@qq.com>
2018-01-29 17:37:34 +0800

    yd_oral_single_zh2ko

--- Diglossia/scrapyProject/scrapyProject/settings.py
+++ Diglossia/scrapyProject/scrapyProject/settings.py
@@ -141,8 +141,8 @@ ITEM_PIPELINES = {
 # Mysql数据库的配置信息
 # MYSQL_HOST = '10.146.254.57'
 # MYSQL_HOST = '10.146.252.112'
-MYSQL_HOST = '10.142.237.97'
-# MYSQL_HOST = '106.39.246.223'
+# MYSQL_HOST = '10.142.237.97'
+MYSQL_HOST = '106.39.246.223'
 MYSQL_DBNAME = 'spider'  # 数据库名字，请修改
 MYSQL_USER = 'spider'  # 数据库账号，请修改
 MYSQL_PASSWD = 'chenguang'  # 数据库密码，请修改
